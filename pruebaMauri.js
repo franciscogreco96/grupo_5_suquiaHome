29ece3b791dc65047d688a83f5ae64a84e149318
franciscogreco96 <franciscogreco96@gmail.com>
2023-01-28 20:21:26 -0300

    segunda prueba modificacion pruebaMauri.js

--- pruebaMauri.js
+++ pruebaMauri.js
@@ -2,4 +2,5 @@ console.log("Buenass");
 console.log("Prueba de commit");
 
 
-console.log("probando push modificacion! de pruebaMauri.js")
+console.log("probando push modificacion! de pruebaMauri.js");
+console.log("segunda prueba");
