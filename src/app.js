934587d0b7b432171079b866bee3f507753e00d2
Mauri Chocholacek <maurichocholacek.git@gmail.com>
2023-02-20 09:40:59 -0300

    Agregando detalle de producto mobile first html 80%

--- src/app.js
+++ src/app.js
@@ -9,3 +9,9 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "./views/index.html"))
 });
+
+/* Ruta para ingreso a detalle de producto */
+app.get('/productDetail.html', function(req, res){
+    res.sendFile(path.join(__dirname, "./views/productDetail.html"))
+});
+
