69a8c7c3848ef84e1ab0e5a619e1267b850b5f12
Mauri Chocholacek <maurichocholacek.git@gmail.com>
2023-02-24 14:56:35 -0300

    Agregando nav/header temporal y linkeo a todas las secciones

--- src/app.js
+++ src/app.js
@@ -20,3 +20,12 @@ app.get("/productCart.html", function(req, res){
     res.sendFile(path.join(__dirname, "./views/productCart.html"))
 });
 
+/* Ruta para ingreso a login */
+app.get("/login.html", function(req, res){
+    res.sendFile(path.join(__dirname, "./views/login.html"))
+});
+
+/* Ruta para ingreso a register */
+app.get("/register.html", function(req, res){
+    res.sendFile(path.join(__dirname, "./views/register.html"))
+});
