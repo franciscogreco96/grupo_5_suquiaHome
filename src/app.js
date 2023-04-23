dab53869ed1ca37f40aabda15a358c9280809061
Mauri Chocholacek <maurichocholacek.git@gmail.com>
2023-04-23 14:45:55 -0300

    Intento de login y session

--- src/app.js
+++ src/app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const methodOverride = require("method-override");
+const session = require("express-session");
 
+app.use(session({
+    secret: 'Hola que tal',
+    resave: false,
+    saveUninitialized: false,
+}))
 /* importando los enrutadores */
 const mainRouter = require("./routes/mainRouter.js");
 const productRouter = require("./routes/productRouter.js");
@@ -13,6 +19,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(methodOverride("_method"));
+
 
 /* usando el motor de plantillas */
 app.set("view engine", "ejs");
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,10 +2,12 @@ const path= require('path');
 const { validationResult } = require("express-validator")
 const User = require("../models/Users")
 const bcryptjs = require("bcryptjs");
+const { log } = require('console');
 
 const userController={
     login: (req,res)=>{
          res.render('user/login');
+
     },
     register: (req,res)=>{
          res.render('user/register');
@@ -18,6 +20,23 @@ const userController={
           oldData: req.body
      });
        }
+
+       /* Verificando si el email no esa registrado */
+       let userInDB= User.findByField('email',req.body.email);
+
+       if(userInDB){
+         return res.render('user/register', {
+         errors: {
+               email: {
+                     msg: 'Este email ya esta registrado'
+               }
+            },
+            /* queda doble el campo email al encontrar un error */
+            
+          oldData: req.body
+         });
+      }
+       
        let userToCreate = {
           ...req.body,
           /* contraseña: bcryptjs.hashSync(req.body.contraseña, 10), */
@@ -25,10 +44,43 @@ const userController={
        }
 
        User.create(userToCreate);
-       return res.send("ok ! se guardó el usuario");
+       return res.render('user/profile', req.body);
+    },
+
+   
+
+    /* min 40 a 60 aprox de video, no aparecen los errores ni envia a la vista de perfil, solo recarga el login */
+    processLogin: (req,res) =>{
+      let userToLogin= User.findByField('email',req.body.email);
+
+      if(userToLogin){
+
+         /*  CHEQUEO DE PASSWORD ENCRIPTADA */
+         /* let chequeoPassword= bcryptjs.compareSync(req.body.password,userToLogin.password);
+
+         if(chequeoPassword){
+         return res.redirect('user/profile')
+         } */
+
+         req.session.userLogged=userToLogin;
+
+         return res.redirect('/user/profile')
+      }
+
+      return res.render('user/login', {
+   errors: {
+      email: { 
+         msg: 'Este email no esta registrado'
+      }
+   }
+})
     },
     profile: (req,res)=>{
-     res.render('user/profile');
-    }
+      console.log("Estas en profile");
+      console.log(req.session);
+      res.render('user/profile',{
+         user: req.session.userLogged
+      });
+     }
 }
 module.exports = userController;
